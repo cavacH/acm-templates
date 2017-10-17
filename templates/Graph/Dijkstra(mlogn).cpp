c6cfe2c95dc24a8d507835967add1107d6db38e5
hhd <hhd1997@sina.cn>
2017-10-17 10:55:27 +0800

    dij

--- templates/Graph/Dijkstra(mlogn).cpp
+++ templates/Graph/Dijkstra(mlogn).cpp
@@ -1,61 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define clr(x) memset(x,0,sizeof(x))
-#define LL long long
-#define pll pair<long long,long long>
-#define pii pair<int,int>
-#define mkp make_pair
-#define pb push_back
+#define LL long long 
+#define pli pair<LL, int>
 
 const int N = 1005;
-const int INF = 0x3f3f3f3f;
+const LL INF = 0x7f7f7f7f7f7f7f7fLL;
 
-struct Edge{
-    int from,to,dist;
-    Edge(int u,int v,int d):
-        from(u),to(v),dist(d){}
-};
+vector<pli> g[N];
+int n;
+LL d[N];
+bool done[N];
 
-struct Dijkstra{
-    int n,m;
-    vector<Edge> edges;
-    vector<int> G[N];
-    bool done[N];
-    int d[N],p[N];
-
-    void init(int n){
-        this->n=n;
-        for(int i=1;i<=n;i++) G[i].clear();
-        edges.clear();
-    }
-
-    void AddEdge(int from,int to,int dist){
-        edges.pb(Edge(from,to,dist));
-        m=edges.size();
-        G[from].pb(m-1);
-    }
-
-    void dijkstra(int s){
-        priority_queue<pii,vector<pii>,greater<pii> > Q;
-        for(int i=1;i<=n;i++) d[i]=INF;
-        d[s]=0;
-        clr(done);
-        Q.push(mkp(0,s));
-        while(!Q.empty()){
-            pii x=Q.top();
-            Q.pop();
-            int u=x.second;
-            if(done[u]) continue;
-            done[u]=1;
-            for(int i=0;i<G[u].size();i++){
-                Edge e=edges[G[u][i]];
-                if(d[e.to]>d[u]+e.dist){
-                    d[e.to]=d[u]+e.dist;
-                    p[e.to]=G[u][i];
-                    Q.push(mkp(d[e.to],e.to));
-                }
+void dij(int s) {
+    priority_queue<pli, vector<pli>, greater<pli> > Q;
+    memset(d, 0x7f, sizeof(d));
+    memset(done, 0, sizeof(done));
+    d[s] = 0;
+    Q.push(make_pair(0, s));
+    while(!Q.empty()) {
+        int u = Q.top().second;
+        Q.pop();
+        if(done[u]) continue;
+        done[u] = true;
+        for(auto &x : g[u]) {
+            int v = x.second;
+            LL w = x.first;
+            if(d[v] > d[u] + w) {
+                d[v] = d[u] + w;
+                Q.push(make_pair(d[v], v));
             }
         }
     }
-};
+}
+
